0f32902be8658c5c5697dd26aa147ab1f4c87e19
Gau o fthe Veldt <gau_veldt@hotmail.com>
2016-08-04 22:19:14 -0700

    Giving player object ability to actually be in a world.

--- server/factory/Player.gd
+++ server/factory/Player.gd
@@ -1,7 +1,8 @@
 
 extends "GOB.gd"
 
-var name
+var name		# player name
+var region		# null for overworld, otherwise names region player is in
 
 func type():
 	return "Player"
@@ -9,13 +10,33 @@ func type():
 func on_read(key):
 	if key=="name":
 		return [true,name]
+	if key=="region":
+		return [true,region]
+	if key=="pos":
+		var tr=get_translation()
+		return [true,[tr.x,tr.y,tr.z]]
+	if key=="rot":
+		var rot=get_rotation_deg()
+		return [true,[rot.x,rot.y,rot.z]]
 	return [false,null]
 
 func on_write(key,val):
 	if key=="name":
 		name=val
 		return true
+	if key=="region":
+		region=val
+		return true
+	if key=="pos":
+		set_translation(Vector3(val[0],val[1],val[2]))
+		return true
+	if key=="rot":
+		set_rotation_deg(Vector3(val[0],val[1],val[2]))
+		return true
 	return false
 
 func _ready():
 	write("name","NoName")
+	write("region",null)
+	write("pos",[0,0,0])
+	write("rot",[0,0,0])
